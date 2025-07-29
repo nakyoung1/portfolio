32d5ac42c8fcf1b30fac4487fca1673c69db4c21
nakyoung1 <skrud5730@naver.com>
2025-07-29 10:30:41 +0900

    333

--- index.js
+++ index.js
@@ -1,93 +1,96 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+var __awaiter =
+  (this && this.__awaiter) ||
+  function (thisArg, _arguments, P, generator) {
+    function adopt(value) {
+      return value instanceof P
+        ? value
+        : new P(function (resolve) {
+            resolve(value);
+          });
+    }
     return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
+      function fulfilled(value) {
+        try {
+          step(generator.next(value));
+        } catch (e) {
+          reject(e);
+        }
+      }
+      function rejected(value) {
+        try {
+          step(generator["throw"](value));
+        } catch (e) {
+          reject(e);
+        }
+      }
+      function step(result) {
+        result.done
+          ? resolve(result.value)
+          : adopt(result.value).then(fulfilled, rejected);
+      }
+      step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
-};
+  };
 $(function () {
-    // 스크롤 이벤트 감지
-    $(window).on("scroll", function () {
-        if ($(this).scrollTop() > 100) {
-            $("#topButton").fadeIn(); // 버튼 나타남
-        }
-        else {
-            $("#topButton").fadeOut(); // 버튼 사라짐
-        }
-    });
-    // 버튼 클릭 시 최상단으로 이동
-    $("#topButton").on("click", function () {
-        $("html, body").animate({ scrollTop: 0 }, 300); // 0.5초(500ms) 동안 스크롤 업
-    });
-    $("header p").on("click", function () {
-        $("html, body").animate({ scrollTop: 0 }, 300); // 0.5초(500ms) 동안 스크롤 업
-    });
-    $(window).on("scroll", function () {
-        if ($(this).scrollTop() > 100) {
-            $("nav").css({
-                "background-color": "#eee",
-                color: "#111",
-            });
-            $("#header-list a").css({ color: "#111" });
-            $("#header-list a")
-                .on("mouseenter", function () {
-                // 마우스를 올렸을 때
-                $(this).css("color", "#8d5d5d");
-            })
-                .on("mouseleave", function () {
-                // 마우스를 뗐을 때
-                $(this).css("color", "#111");
-            });
-            $("#weather-container").css({
-                display: "none",
-            });
-        }
-        else {
-            $("nav").css({
-                "background-color": "transparent",
-                color: "#eee",
-            });
-            $("#header-list a").css({ color: "#eee" });
-            $("#header-list a")
-                .on("mouseenter", function () {
-                // 마우스를 올렸을 때
-                $(this).css("color", "#8d5d5d");
-            })
-                .on("mouseleave", function () {
-                // 마우스를 뗐을 때
-                $(this).css("color", "#eee");
-            });
-            $("#weather-container").css({
-                display: "block",
-            });
-        }
-    });
-    $(".project-box").on("click", function () {
-        var link = $(this).find("a").attr("href"); // <a> 태그 안의 href 값을 가져옴
-        if (link) {
-            window.open(link, "_blank"); // 새 창에서 링크 열기
-        }
-    });
+  // 스크롤 이벤트 감지
+  $(window).on("scroll", function () {
+    if ($(this).scrollTop() > 100) {
+      $("#topButton").fadeIn(); // 버튼 나타남
+    } else {
+      $("#topButton").fadeOut(); // 버튼 사라짐
+    }
+  });
+  // 버튼 클릭 시 최상단으로 이동
+  $("#topButton").on("click", function () {
+    $("html, body").animate({ scrollTop: 0 }, 300); // 0.5초(500ms) 동안 스크롤 업
+  });
+  $("header p").on("click", function () {
+    $("html, body").animate({ scrollTop: 0 }, 300); // 0.5초(500ms) 동안 스크롤 업
+  });
+  $(window).on("scroll", function () {
+    if ($(this).scrollTop() > 100) {
+      $("nav").css({
+        "background-color": "#eee",
+        color: "#111",
+      });
+      $("#header-list a").css({ color: "#111" });
+      $("#header-list a")
+        .on("mouseenter", function () {
+          // 마우스를 올렸을 때
+          $(this).css("color", "#8d5d5d");
+        })
+        .on("mouseleave", function () {
+          // 마우스를 뗐을 때
+          $(this).css("color", "#111");
+        });
+      $("#weather-container").css({
+        display: "none",
+      });
+    } else {
+      $("nav").css({
+        "background-color": "transparent",
+        color: "#eee",
+      });
+      $("#header-list a").css({ color: "#eee" });
+      $("#header-list a")
+        .on("mouseenter", function () {
+          // 마우스를 올렸을 때
+          $(this).css("color", "#8d5d5d");
+        })
+        .on("mouseleave", function () {
+          // 마우스를 뗐을 때
+          $(this).css("color", "#eee");
+        });
+      $("#weather-container").css({
+        display: "block",
+      });
+    }
+  });
+  $(".project-box").on("click", function () {
+    var link = $(this).find("a").attr("href"); // <a> 태그 안의 href 값을 가져옴
+    if (link) {
+      window.open(link, "_blank"); // 새 창에서 링크 열기
+    }
+  });
 });
-const weatherCity = document.getElementById("weather-city");
-const weather = document.getElementById("weather");
-const weatherIcon = document.getElementById("weather-icon");
-function getTemp() {
-    navigator.geolocation.getCurrentPosition((position) => __awaiter(this, void 0, void 0, function* () {
-        let lat = position.coords.latitude;
-        let long = position.coords.longitude;
-        const response = yield fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=034db30f421b1daff5ebe414f46fc87c&units=metric&lang=kr`);
-        const data = yield response.json();
-        const iconCode = data.weather[0].icon;
-        const url = `https://openweathermap.org/img/wn/${iconCode}.png`;
-        console.log(data);
-        //구조분해
-        weatherIcon.innerHTML = `<img src="${url}" alt="Weather Icon">`;
-        weather.innerHTML = `온도 : ${data.main.temp}℃<br>습도 : ${data.main.humidity}`;
-        weatherCity.innerHTML = data.name;
-    }));
-}
-getTemp();
